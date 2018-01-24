8f58658d3ecec2ee97432d3cab34d746150b4bd8
BUGMINI-PC\Айнур <aynuralmurzina@gmail.com>
2018-01-24 18:26:26 +0600

    Player movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+	[SerializeField]
+	protected float speed;
+	protected Vector2 direction;
+
+	private Animator animator;
+
+	void Start(){
+		animator = GetComponent<Animator> ();
+	}
+
+	protected virtual void FixedUpdate(){
+		Move ();
+
+		if(direction.x != 0 || direction.y != 0)
+			AnimateMovement ();
+		else
+			animator.SetLayerWeight (1, 0f);
+	}
+
+	protected void Move(){
+		transform.Translate (direction * speed * Time.deltaTime);
+	}
+
+	protected void AnimateMovement(){
+
+		animator.SetLayerWeight (1, 1f);
+
+		animator.SetFloat ("x", direction.x);
+		animator.SetFloat ("y", direction.y);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	protected override void FixedUpdate(){
+		GetInput ();	
+		base.FixedUpdate ();
+	}
+
+	void GetInput(){
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		direction = new Vector2 (x, y);
+	}
+}
