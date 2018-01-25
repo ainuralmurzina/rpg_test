5c506815cd86b67babe9fad03f3b9cf11d18b705
BUGMINI-PC\Айнур <aynuralmurzina@gmail.com>
2018-01-25 18:09:23 +0600

    Code and animation refactoring

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,30 +8,41 @@ public abstract class Character : MonoBehaviour {
 	protected float speed;
 	protected Vector2 direction;
 
+	private Rigidbody2D rigidbody;
 	private Animator animator;
+
+	protected bool IsMoving{
+		get { return direction.x != 0 || direction.y != 0; }
+	}
 
 	protected virtual void Start(){
 		animator = GetComponent<Animator> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
 	}
 
 	protected virtual void FixedUpdate(){
 		Move ();
-
-		if(direction.x != 0 || direction.y != 0)
-			AnimateMovement ();
-		else
-			animator.SetLayerWeight (1, 0f);
+		HandleLayers ();
 	}
 
 	protected void Move(){
-		transform.Translate (direction * speed * Time.deltaTime);
+		rigidbody.velocity = direction.normalized * speed;
 	}
 
-	protected void AnimateMovement(){
-
-		animator.SetLayerWeight (1, 1f);
-
-		animator.SetFloat ("x", direction.x);
-		animator.SetFloat ("y", direction.y);
+	void HandleLayers (){
+		if (IsMoving) {
+			ActivateLayer("WalkLayer");
+			animator.SetFloat ("x", direction.x);
+			animator.SetFloat ("y", direction.y);
+		}
+		else
+			ActivateLayer("IdleLayer");
 	}
+
+	void ActivateLayer(string layerName){
+		for (int i = 0; i < animator.layerCount; i++)
+			animator.SetLayerWeight (i, 0f);
+		animator.SetLayerWeight (animator.GetLayerIndex(layerName), 1f);
+	}
+
 }
